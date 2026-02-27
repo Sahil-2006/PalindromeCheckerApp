c4cff2bbd33230355e410df0d9fde6e5b45f86be
Sahil 2006 <sahilshabbirharyani@gmail.com>
2026-02-27 13:11:10 +0300

    [Sahil] Updated usercase1 palindromechecker

---
+++ PalindromeCheckerApp.java
@@ -0,0 +1,6 @@
+public class PalindromeCheckerApp{
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Palindrome Checker Management System Version : 1.0");
+        System.out.println("System Initialised successfully");
+    }
+}
